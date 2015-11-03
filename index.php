f719a0da8fd8a033eb6cd457a599ac3861060092
unknown <linuxtheblack@hotmail.com>
2015-11-03 13:50:02 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Hello</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<p>My milkshake brings all the boys to the yard!</p>
+</body>
+</html>
+<?php
+$dir = dirname(__FILE__);
+echo "<p>Full path to this dir: " . $dir . "</p>";
+echo "<p>Full path to a .htpasswd file in this dir: " . $dir . "/.htpasswd" . "</p>";
+?>
